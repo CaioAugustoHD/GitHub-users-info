55e2ed89bccf99a850bb658946b74335dc5d7741
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-12-26 14:25:53 -0300

    Criado arquivo com tema para o projeto

---
+++ src/styles/theme/default.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from "styled-components";
+
+export const defaultTheme : DefaultTheme = {
+
+    body: '#F2F2F2',
+    main: '#A8DADC',
+    inside: '#457B9D',
+    dateils: '#1D3557',
+    text: '#000'
+    
+}
