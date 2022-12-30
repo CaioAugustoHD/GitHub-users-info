06193012a78f1b8acc43a465aefdcbdd0e914751
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-12-30 13:10:16 -0300

    Criado arquivo style para componente Profile

--- src/components/Profile/style.ts
+++ src/components/Profile/style.ts
@@ -1,17 +1,20 @@
 import styled  from "styled-components";
 
 export const ProfileContainer = styled.section`
-
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `;
 
 export const Avatar = styled.img`
-
-`;
-
-export const UserName = styled.span`
-
+    width: 190px;
+    margin: 0.5em;
 `;
 
 export const Bio = styled.span`
+    margin-bottom: 0.5em;
+`;
 
+export const UserName = styled(Bio)`
+    font-size: 1.5em
 `;
