3a6a4c14a683e7827f091db77819ec88dd4b7f6f
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-12-30 11:25:56 -0300

    Mudanças em App.tsx: chamada da Api e dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,52 @@
-import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/GlobalStyle';
+import { defaultTheme } from './styles/theme/default';
 
+import { Profile } from './components/Profile';
+import { Container } from './styles/Container';
+import { api } from './lib/axios';
+import { useEffect, useState } from 'react';
+
+interface ApiResponse  {
+  avatar_url: string,
+  name: string,
+  bio: string
+}
 
 export function App() {
+
+  const [apiData, setApiData] = useState<ApiResponse>({} as ApiResponse)
+
+  async function ApiFetch(userName: string) {
+    
+    try {
+
+    const response = await api.get(`/${userName}`);
+    const data = response.data as ApiResponse;
+    setApiData(data);
+
+    } catch(err) {
+      console.log('Aconteceu um erro -> ' + err);
+    }
+
+  }
+
+  useEffect(() => {
+    ApiFetch('CaioAugustoHD')
+  }, []);
+
   return (
-    <h1>Hello</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+
+      <Container>
+        <Profile 
+          avatar = {apiData.avatar_url}
+          name = {apiData.name}
+          bio = {apiData.bio}
+          />
+      </Container>
+      
+    </ThemeProvider>
   )
 }
