3d53c434d4a9c49e68ae2e86212e7902c37810d8
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-12-30 11:23:33 -0300

    Criado container.ts com estilização do conteiner da página

---
+++ src/styles/Container.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+
+    width: 50%;
+    margin: auto;
+    border: 1px solid #000;
+
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
