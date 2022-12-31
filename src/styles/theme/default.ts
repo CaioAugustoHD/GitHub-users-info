4e16d978a021fdbe669d571e514b81023608bbe2
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-12-30 21:02:20 -0300

    Correção da palavra details

--- src/styles/theme/default.ts
+++ src/styles/theme/default.ts
@@ -5,7 +5,7 @@ export const defaultTheme : DefaultTheme = {
     body: '#F2F2F2',
     main: '#A8DADC',
     inside: '#457B9D',
-    dateils: '#1D3557',
+    details: '#1D3557',
     text: '#000'
     
 }
